f531b41143e168bf003704a94eacf84e351f335f
Itai Ganot <itai.ganotx@foretellix.com>
2020-12-03 13:05:23 +0200

    Updates output of cluster name

--- tf-eks_cluster/output.tf
+++ tf-eks_cluster/output.tf
@@ -3,5 +3,9 @@ output "eks_cluster_endpoint" {
 }
 
 output "eks_cluster_certificate_authority" {
-  value = aws_eks_cluster.aws_eks.certificate_authority 
+  value = aws_eks_cluster.aws_eks.certificate_authority
+}
+
+output "eks_cluster_name" {
+  value = aws_eks_cluster.aws_eks.id
 }
